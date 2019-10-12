9a38a9b6e7f27db5b8de1cb91af617fe6878ceee
David Diaz <daemdisa@aol.com>
2019-10-11 21:00:31 -0500

    working

--- src/App.js
+++ src/App.js
@@ -9,24 +9,28 @@ class App extends Component {
       <div className="App">
         <main className="flexbox">
           <Board id="board-1" className="board">
+            <p className="info">One</p>
             <Card id="card-1" className="card" draggable="true">
               <p>Card one</p>
             </Card>
           </Board>
 
           <Board id="board-2" className="board">
+          <p className="info">Two</p>
             <Card id="card-2" className="card" draggable="true">
               <p>Card two</p>
             </Card>
           </Board>
 
           <Board id="board-3" className="board">
+          <p className="info">Three</p>
             <Card id="card-3" className="card" draggable="true">
               <p>Card three</p>
             </Card>
           </Board>
 
           <Board id="board-4" className="board">
+          <p className="info">Four</p>
             <Card id="card-4" className="card" draggable="true">
               <p>Card four</p>
             </Card>
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 
 function Board(props) {
+    
 
     const drop = e => {
         e.preventDefault();
